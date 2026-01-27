d0614a197ece1f0f36a81082262dc2ee91cc1cb7
서제경 <seojk0315@naver.com>
2026-01-27 21:42:15 +0900

    fix: chat 라우터 추가

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -8,9 +8,10 @@ import Login from '@/pages/auth/Login'
 import MainLayout from '@/layout/MainLayout'
 import CustomizeLayout from '@/layout/CustomizeLayout'
 
-import Chat from '@/pages/Chat'
 import Order from '@/pages/Order'
 import Customize from '@/pages/customize/Customize'
+import ChatList from '@/pages/chat/ChatList'
+import ChatRoom from '@/pages/chat/ChatRoom'
 
 const router = createBrowserRouter([
   {
@@ -20,9 +21,13 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> },
       { path: 'map', element: <Map /> },
-      { path: 'chat', element: <Chat /> },
+      { path: 'chat', element: <ChatList /> },
       { path: 'order', element: <Order /> },
     ],
+  },
+  {
+    path: '/chat/:chatId',
+    element: <ChatRoom />,
   },
   {
     path: '/customize',
